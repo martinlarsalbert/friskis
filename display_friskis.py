41b768d1ba5d8cd7d93c30ddda1bbf184d9b1b98
martinlarsalbert <marale@kth.se>
2018-05-13 21:07:56 +0200

    again

--- display_friskis.py
+++ display_friskis.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from pyvirtualdisplay import Display
 
 with Display():
--- friskis.py
+++ friskis.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver as webdriver
 
 import os.path
