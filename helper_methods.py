e0eff119092bda4d65e621393d80633f01a41e11
martinlarsalbert <marale@kth.se>
2018-05-14 21:52:41 +0200

    heheh

--- helper_methods.py
+++ helper_methods.py
@@ -160,22 +160,22 @@ def book(browser,day,search_words):
     logging.info('Day button name is:%s' % day_button_name)
 
     logging.info('Find the day button...')
-    #day_buttons = find_day_buttons(browser=browser)
+    day_buttons = find_day_buttons(browser=browser)
     #day_buttons = browser.find_elements_by_class_name("DayButton")
 
-    #ok = False
-    #for i,day_button in enumerate(day_buttons):
-    #    if day_button.text == day_button_name:
-    #        ok = True
-    #        real_day_button = day_button
-    #        break
+    ok = False
+    for i,day_button in enumerate(day_buttons):
+        if day_button.text == day_button_name:
+            ok = True
+            real_day_button = day_button
+            break
 
-    #if not ok:
-    #    raise ValueError('Cannot find %s button' % day_button_name)
+    if not ok:
+        raise ValueError('Cannot find %s button' % day_button_name)
 
 
     #pdb.set_trace()
-    real_day_button = browser.find_element_by_xpath(r'//*[@id="viewtabcontrol"]/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[1]/div')
+    #real_day_button = browser.find_element_by_xpath(r'//*[@id="viewtabcontrol"]/div/div[2]/div/div/div[1]/div/div/div[2]/div/div/div[1]/div')
 
     logging.info('The following day buttons have been found: %s' % real_day_button.text)
     #time.sleep(5)
